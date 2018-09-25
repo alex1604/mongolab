2fde77a550810aaf41c2aa31b1022f9af1081286
Alejandro <92.alejandrogarcia@gmail.com>
2018-09-25 15:03:52 +0200

    add feature 1.3, 2.1, 2.2, beginning 2.3

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://127.0.0.1:27017';
 const databaseName = 'mongolab';
 const collectionName = 'products';
+const pictureCollection = 'pictures';
+const querylabName = 'querylab';
 
 console.log('About to connect to MongoDB');
 
@@ -20,6 +22,17 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
                 return;
             }
             console.log('The products are: ', docs);
+        })
+    }
+    var showMeOnly = function (amount, collection){
+        collection.find({}).toArray((err, docs) => {
+            client.close();
+            console.log('Connection closed.');
+            if (err) {
+                console.log('Could not use query find: ', err);
+                return;
+            }
+            console.log('The products are: ', docs.slice(0, amount));
         })
     }
     var generateData = function (times) {
@@ -45,28 +58,42 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
         }
         return myArray
     }
-    var deleteData = function (collection, filter) {
+    var deleteData = function (collection, filter, callback) {
         showMeAll(collection);
 
         collection.deleteMany(
             filter,
             (err, result) => {
                 // result är ett document med statusinformation
-                //console.log(result);
+                console.log('All products were deleted.');
             }
         );
         showMeAll(collection);
+        callback(8,pictures);
     }
 
     const mongolab = client.db(databaseName);
     console.log('We are connected to the mongolabdb');
-    const products = mongolab.collection(collectionName)
+    const products = mongolab.collection(collectionName);
+    const pictures = mongolab.collection(pictureCollection)
+    const querylab = mongolab.collection(querylabName);
     const filterAll = {}
     let callback = () => {
-        deleteData(products, filterAll)
+        deleteData(products, filterAll, showMeOnly)
     }
-
     products.insertMany(generateData(15), callback)
-    
+    querylab.insertMany(generateData(10000));
 })
+
+// 2.2 Shell Queries: 
+/*
+    Power Shell > mongo > use mongolab
+
+    1. db.querylab.count()
+    2. db.querylab.createIndex({price: -1})
+       db.querylab.aggregate([{$group: {_id: null,antal: { $sum: 1 },summa: {$sum: "$price"}}}])
+    3.
+    Sortera produkterna i första hand fallande efter pris, i andra hand 
+    i stigande bokstavsordning efter namn.
+*/
 
